8b8c4900e5c5291ca2ae69d95c6468556c21413a
Schimith <schimith.guilherme@gmail.com>
2026-03-03 23:04:17 -0300

    plugin v1

---
+++ zsh-tradutor.plugin.zsh
@@ -0,0 +1,27 @@
+# tradutor portugues inglês
+
+function tra() {
+
+  input="$*"
+  query="${input// /+}"
+
+  if [[ "$1" == "en" ]]; then
+    input="${@[2,-1]}"
+    query="${input// /+}"
+    url="https://translate.google.com/m?sl=en&tl=pt&q=${query}"
+  else
+    url="https://translate.google.com/m?sl=pt&tl=en&q=${query}"
+  fi 
+  
+  traducao=$(curl -s -A "Mozilla/5.0" "$url" | \
+    sed -n 's/.*<div[^>]*class="result-container"[^>]*>\([^<]*\)<\/div>.*/\1/p')
+
+  if [[ -n "$traducao" ]]; then
+    echo "•••••"
+    echo "tradução: $traducao"
+    echo "•••••"
+  else
+    echo "Erro: não foi possivel traduzir"
+  fi
+
+}
